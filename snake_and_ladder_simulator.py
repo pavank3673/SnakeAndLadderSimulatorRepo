ccbb860a1ab06b21bebef324fec0f58fbdbe685c
pavank3673 <pavank3673@outlook.com>
2024-12-23 10:48:19 +0530

    [Pavan Kumar]Added:repeat simulation till player reaches 100

--- snake_and_ladder_simulator.py
+++ snake_and_ladder_simulator.py
@@ -3,17 +3,21 @@ from random import randint
 def snake_and_ladder():
     input("Hit enter to start snake and ladder")
     position = 0 
-    input("Roll the die")
-    die_value = randint(1, 6)
-    check_option = randint(1, 3)
-    if check_option == 1:
-        print("check option : No Play")
-    elif check_option == 2:
-        print("check option : Ladder")
-        position += die_value
-    elif check_option == 3:
-        print("check option : Snake")
-        position -= die_value
+    while(position != 100):
+        input("Roll the die")
+        die_value = randint(1, 6)
+        check_option = randint(1, 3)
+        if check_option == 1:
+            print("check option : No Play")
+        elif check_option == 2:
+            print("check option : Ladder")
+            position += die_value
+        elif check_option == 3:
+            print("check option : Snake")
+            position -= die_value
+            if position < 0:
+                position = 0
+        print(position)
 
 
 snake_and_ladder()
