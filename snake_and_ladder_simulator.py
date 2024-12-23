377dbc14adaf44d7ef43244a459b3c0f696faf4a
pavank3673 <pavank3673@outlook.com>
2024-12-23 10:39:15 +0530

    [Pavan Kumar]Added:player roll the die

--- snake_and_ladder_simulator.py
+++ snake_and_ladder_simulator.py
@@ -1,6 +1,10 @@
+from random import randint
 
 def snake_and_ladder():
     input("Hit enter to start snake and ladder")
     position = 0
+    input("Roll the die")
+    die_value = randint(1, 6)
+        
 
 snake_and_ladder()
